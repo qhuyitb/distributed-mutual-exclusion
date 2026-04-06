eaf6cacfed296cc7237e7e8cb66a0826cb2c313a
Y0KUNA <lequanghuy18062004@gmail.com>
2026-04-06 21:21:45 +0700

    Delete src directory

--- src/config.py
+++
@@ -1,6 +0,0 @@
-# Configuration settings for the Token Ring network
-
-NUM_NODES = 4  # Number of nodes in the Token Ring network
-HOST = '127.0.0.1'  # Host address for the nodes
-PORT = 5000  # Port number for the TCP sockets
-TIMEOUT = 1  # Timeout for socket operations in seconds
